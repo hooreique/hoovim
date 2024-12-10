599f4e96754ebe6fedf91b75b1d7586395e3d282
hooreique <46372718+hooreique@users.noreply.github.com>
2024-12-10 16:50:00 +0900

    add mini.comment
    and mini.diff `g` 키 매핑 정상화

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,10 +2,37 @@ return {
   {
     'echasnovski/mini.nvim',
     config = function()
-      require('mini.diff').setup {}
+      require('mini.diff').setup {
+        mappings = {
+          apply = '',
+          reset = '',
+          textobject = '',
+          goto_first = '',
+          goto_prev = '',
+          goto_next = '',
+          goto_last = '',
+        }
+      }
+
       require('mini.git').setup {}
+
       require('mini.icons').setup {}
+
       require('mini.statusline').setup {}
+
+      require('mini.comment').setup {
+        mappings = {
+          -- Toggle comment for both Normal and Visual modes
+          comment = '',
+          -- Toggle comment on current line
+          comment_line = '/',
+          -- Toggle comment on visual selection
+          comment_visual = '/',
+          -- Define 'comment' textobject
+          -- Works also in Visual mode if mapping differs from `comment_visual`
+          textobject = '/',
+        }
+      }
     end,
   },
 }
