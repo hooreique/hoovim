982b2c352243ab49b01db410aea2296432d39ac8
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2024-12-11 22:13:01 +0900

    use ivy

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,9 +7,21 @@ return {
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     config = function()
-      local ts = require 'telescope.builtin'
+      require('telescope').setup {
+        pickers = {
+          find_files = { theme = 'ivy' },
+          live_grep = { theme = 'ivy' },
+          buffers = { theme = 'ivy' },
+          help_tags = { theme = 'ivy' },
+        },
+      }
 
-      vim.keymap.set('n', '<Space>ad', ts.find_files)
+      local bi = require 'telescope.builtin'
+
+      vim.keymap.set('n', '<Space>if', bi.find_files)
+      vim.keymap.set('n', '<Space>ig', bi.live_grep)
+      vim.keymap.set('n', '<Space>ib', bi.buffers)
+      vim.keymap.set('n', '<Space>ih', bi.help_tags)
     end,
   },
 }
