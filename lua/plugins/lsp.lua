9b20cd1fcbfdd9c8d14cb7a8e3dee3ea6511c619
hooreique <46372718+hooreique@users.noreply.github.com>
2024-12-11 15:29:31 +0900

    update lspconfig
    add lazydev to dep in lspconfig for lua code
    remove .luarc.jsonc

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,6 +15,19 @@ end
 return {
   {
     'neovim/nvim-lspconfig',
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
     config = function()
       local lsp = require 'lspconfig'
 
