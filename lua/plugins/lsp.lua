e9a1debdab40039f93b48bafacda8ce57fc167ed
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2024-12-20 21:20:53 +0900

    add blink.cmp config (#1)
    * add blink
    
    * add blink.cmp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,23 +1,8 @@
-local remap = function(_, bufnr)
-  local opts = {
-    buffer = bufnr,
-    noremap = true,
-    nowait = true,
-    silent = true,
-  }
-
-  vim.keymap.set('n', 'a',        vim.lsp.buf.references,    opts)
-  vim.keymap.set('n', 'b',        vim.lsp.buf.definition,    opts)
-  vim.keymap.set('n', 'B',        vim.lsp.buf.hover,         opts)
-  vim.keymap.set('n', 'k',        vim.lsp.buf.rename,        opts)
-  vim.keymap.set('n', '<Space>f', vim.lsp.buf.format,        opts)
-  vim.keymap.set('n', '<Space>d', vim.diagnostic.open_float, opts)
-end
-
 return {
   {
     'neovim/nvim-lspconfig',
     dependencies = {
+      'saghen/blink.cmp',
       {
         "folke/lazydev.nvim",
         ft = "lua", -- only load on lua files
@@ -32,10 +17,11 @@ return {
     },
     config = function()
       local lsp = require 'lspconfig'
+      local cap = require('blink.cmp').get_lsp_capabilities()
 
-      lsp.denols.setup { on_attach = remap }
-      lsp.lua_ls.setup { on_attach = remap }
-      lsp.nil_ls.setup { on_attach = remap }
+      lsp.denols.setup { capabilities = cap }
+      lsp.lua_ls.setup { capabilities = cap }
+      lsp.nil_ls.setup { capabilities = cap }
     end,
   },
 }
