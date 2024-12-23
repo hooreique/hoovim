b845b80039773d030c7f8d2bfc33c2e00c7e06f6
hooreique <46372718+hooreique@users.noreply.github.com>
2024-12-23 18:40:26 +0900

    update nvim-tree keymap

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -19,15 +19,22 @@ return {
           return { buffer = bufnr, desc = desc, noremap = true, nowait = true, silent = true }
         end
 
+        vim.keymap.set('n', '/', api.tree.toggle_help, opts 'Help')
         vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
         vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts 'Open')
         vim.keymap.set('n', '<Right>', api.node.open.edit, opts 'Open')
         vim.keymap.set('n', '<CR>', api.node.open.edit, opts 'Open')
-        vim.keymap.set('n', 'i', api.node.open.edit, opts 'Open')
         vim.keymap.set('n', '<Left>', api.node.navigate.parent, opts 'Parent Directory')
         vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts 'Close Directory')
-        vim.keymap.set('n', 'n', api.node.navigate.parent_close, opts 'Close Directory')
-        vim.keymap.set('n', 'h', api.tree.collapse_all, opts 'Collapse')
+        vim.keymap.set('n', '-', api.tree.collapse_all, opts 'Collapse')
+
+        vim.keymap.set('n', 'h', '0', opts '0')
+        vim.keymap.set('n', 'o', '$', opts '$')
+
+        vim.keymap.set('n', 'f', 'k', opts 'k')
+        vim.keymap.set('n', 's', 'j', opts 'j')
+        vim.keymap.set('n', 't', api.node.open.edit, opts 'Open')
+        vim.keymap.set('n', 'r', api.node.navigate.parent_close, opts 'Close Directory')
       end,
     }
 
