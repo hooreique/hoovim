8845439d7310a5bb76712cd408a5f4d81d379b63
hooreique <46372718+hooreique@users.noreply.github.com>
2024-12-27 05:09:33 +0900

    update copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,23 @@
+---@type vim.keymap.set.Opts
+local o = { noremap = true, nowait = true }
+
 return {
   'github/copilot.vim',
-  event = 'VeryLazy',
   config = function()
-    vim.cmd 'Copilot setup'
+    vim.keymap.set('n', '<Space>c', '<Nop>', o)
+
+    vim.keymap.set('n', '<Space>cs', function()
+      vim.cmd 'Copilot setup'
+    end, o)
+
+    vim.keymap.set('n', '<Space>cd', function()
+      vim.cmd 'Copilot disable'
+      vim.notify 'Copilot disabled'
+    end, o)
+
+    vim.keymap.set('n', '<Space>ce', function()
+      vim.cmd 'Copilot enable'
+      vim.notify 'Copilot enabled'
+    end, o)
   end,
 }
