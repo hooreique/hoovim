0094b705dea440312f7078c4e2cdddaeecc392f9
hooreique <46372718+hooreique@users.noreply.github.com>
2024-12-30 05:41:38 +0900

    update sonokai colorscheme config

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -6,6 +6,8 @@ return {
     vim.g.sonokai_style = 'atlantis'
     vim.g.sonokai_transparent_background = 1
     vim.g.sonokai_enable_italic = true
+    vim.g.sonokai_diagnostic_line_highlight = 1
+    vim.g.sonokai_diagnostic_virtual_text = 'highlighted'
     vim.cmd.colorscheme 'sonokai'
   end
 }
