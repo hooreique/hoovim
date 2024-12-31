187f1dbbae417a216cca0b0d4732ffbba3a53073
hooreique <46372718+hooreique@users.noreply.github.com>
2024-12-31 23:50:08 +0900

    add copilot.lua

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,28 @@
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup {
+      panel = {
+        enable = false,
+        keymap = {
+          jump_prev = "<C-\\>c",
+          jump_next = "<C-\\>c",
+          accept = "<C-\\>c",
+          refresh = "<C-\\>c",
+          open = "<C-\\>c",
+        },
+      },
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+          next = "<C-\\>c",
+          prev = "<C-\\>c",
+          dismiss = "<C-\\>c",
+        },
+      },
+    }
+  end,
+}
