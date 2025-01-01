369e12c9825f4be137c679f6ce110e1acd1623c0
hooreique <46372718+hooreique@users.noreply.github.com>
2025-01-01 11:18:08 +0900

    rename gitsigns -> gs

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     require('gitsigns').setup {
       on_attach = function(bufnr)
-        local gitsigns = require 'gitsigns'
+        local gs = require 'gitsigns'
 
         ---@param desc string
         ---@return vim.keymap.set.Opts
@@ -20,7 +20,7 @@ return {
           if vim.wo.diff then
             vim.cmd.normal { ']c', bang = true }
           else
-            gitsigns.nav_hunk 'next'
+            gs.nav_hunk 'next'
           end
         end, o 'Next Hunk')
 
@@ -28,27 +28,27 @@ return {
           if vim.wo.diff then
             vim.cmd.normal { '[c', bang = true }
           else
-            gitsigns.nav_hunk 'prev'
+            gs.nav_hunk 'prev'
           end
         end, o 'Prev Hunk')
 
         vim.keymap.set('n', '<Space>;', '<Nop>', o 'Leading NOP')
 
-        vim.keymap.set('n', '<Space>rh', gitsigns.reset_hunk, o 'Reset Hunk')
+        vim.keymap.set('n', '<Space>rh', gs.reset_hunk, o 'Reset Hunk')
         vim.keymap.set('v', '<Space>rh', function()
-          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+          gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
         end, o 'Reset Hunk Range')
-        vim.keymap.set('n', '<Space>;p', gitsigns.preview_hunk, o 'Preview Hunk')
+        vim.keymap.set('n', '<Space>;p', gs.preview_hunk, o 'Preview Hunk')
         vim.keymap.set('n', '<Space>;b', function()
-          gitsigns.blame_line { full = true }
+          gs.blame_line { full = true }
         end, o 'Blame')
-        vim.keymap.set('n', '<Space>;;', gitsigns.toggle_current_line_blame,
+        vim.keymap.set('n', '<Space>;;', gs.toggle_current_line_blame,
           o 'Toggle Line Blame')
-        vim.keymap.set('n', '<Space>;d', gitsigns.diffthis, o 'Diff with Index')
+        vim.keymap.set('n', '<Space>;d', gs.diffthis, o 'Diff with Index')
         vim.keymap.set('n', '<Space>;s', function()
-          gitsigns.diffthis '~'
+          gs.diffthis '~'
         end, o 'Diff with HEAD')
-        vim.keymap.set('n', '<Space>;r', gitsigns.toggle_deleted,
+        vim.keymap.set('n', '<Space>;r', gs.toggle_deleted,
           o 'Toggle Show Deleted')
       end,
     }
