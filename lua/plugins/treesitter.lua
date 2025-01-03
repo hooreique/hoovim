dc2a4ad8378d82f16aa66db96ac9b3daea10b1bd
hooreique <46372718+hooreique@users.noreply.github.com>
2025-01-03 18:53:06 +0900

    refactor: vim.loop -> vim.uv

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -20,8 +20,7 @@ return {
           enable = true,
           disable = function(_, buf)
             local max_filesize = 100 * 1024 -- 100KB
-            ---@diagnostic disable-next-line: undefined-field
-            local ok, stats = pcall(vim.loop.fs_stat,
+            local ok, stats = pcall(vim.uv.fs_stat,
               vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
               return true
