0d799dc98adcc6f71311730b6cc2aa396b3d6ee1
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-03-14 22:10:31 +0900

    update lsp, blink config

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -12,5 +12,17 @@ return {
       completion = { menu = { auto_show = true } },
       keymap = { ['<Tab>'] = { 'show', 'accept' } },
     },
+    sources = {
+      -- add lazydev to your completion providers
+      default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
+        },
+      },
+    },
   },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,23 +18,7 @@ return {
           }
         },
       },
-      {
-        'saghen/blink.cmp',
-        opts = {
-          sources = {
-            -- add lazydev to your completion providers
-            default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-            providers = {
-              lazydev = {
-                name = "LazyDev",
-                module = "lazydev.integrations.blink",
-                -- make lazydev completions top priority (see `:h blink.cmp`)
-                score_offset = 100,
-              },
-            },
-          },
-        },
-      },
+      'saghen/blink.cmp',
       {
         'nvim-java/nvim-java',
         config = function()
